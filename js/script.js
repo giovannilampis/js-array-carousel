0c0425411b5cc9b5a1ac237325e99c865846bb79
giovannilampis <giolampis@hotmail.com>
2023-03-28 15:14:47 +0200

    add starting content to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict"
+
+const prev = document.querySelector(".prev");
+
+const next = document.querySelector(".next");
+
+next.addEventListener("click", function() {
+
+    // selezionare il primo elemento html contenente un'immagine, ovvero quello con classe active
+
+    let activeItem = document.querySelector(".item.active");
+
+    let itemToActive = activeItem.nextElementSibling;
+})
