71c10c802dded2b9f1bb72bbf01306f9431b2566
giovannilampis <giolampis@hotmail.com>
2023-03-28 16:02:59 +0200

    add functionality to slider in script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,39 @@ next.addEventListener("click", function() {
 
     let itemToActive = activeItem.nextElementSibling;
 
+    // make a cycle out of the sliding process with "next"
+
+    if ( activeItem.classList.contains("last") ) {
+        itemToActive = document.querySelector(".item.first");
+    }
+
+    // remove class .active
+
+    activeItem.classList.remove("active");
+
+    // add .active class to the following img element
+
+    itemToActive.classList.add("active");
+
+})
+
+prev.addEventListener("click", function() {
+
+     // get the first html element that contains an img element: the .active classed one
+
+     let activeItem = document.querySelector(".item.active");
+
+     // get the element placed immediately before the one with class .active
+
+    let itemToActive = activeItem.previousElementSibling;
+
+    // make a cycle out of the sliding process with "prev"
+
+
+    if ( activeItem.classList.contains("first") ) {
+        itemToActive = document.querySelector(".item.last");
+    }
+
     // remove class .active
 
     activeItem.classList.remove("active");
